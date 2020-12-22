1106b17d0882524a1d90ccccc8711a05be93a95a
Johannes Poehn <johannes.poehn@gmail.com>
2020-12-22 19:46:57 +0100

    EarthTimeTest

--- MarsMoons.Core.Test/EarthTimeTest.cs
+++ MarsMoons.Core.Test/EarthTimeTest.cs
@@ -10,22 +10,56 @@ namespace MarsMoons.Core.Test
         [MemberData(nameof(GetOperatorPlusTestData))]
         public void OperatorPlus_ReturnsCorrectValue(long startHour, long startMinute, long hoursToAdd, long minutesToAdd)
         {
-            var start = new DateTime(0, 0, 0, (int)startHour, (int)startMinute, 0, 0);
+            var start = DateTime.UnixEpoch + new TimeSpan((int)startHour, (int)startMinute, 0);
             var span = new TimeSpan((int)hoursToAdd, (int)minutesToAdd, 0);
             var end = start + span;
 
-            Assert.Equal(((long)end.Hour, (long)end.Minute), (new PlanetTime<EarthTimeConfiguration>(startHour, startMinute) + new PlanetTimeSpan<EarthTimeConfiguration>(hoursToAdd, minutesToAdd)).ToTime());
-        }
+            var startPlanetTime = new PlanetTime<EarthTimeConfiguration>(startHour, startMinute);
+            var planetTimeSpan = new PlanetTimeSpan<EarthTimeConfiguration>(hoursToAdd, minutesToAdd);
+            var endPlanetTime = startPlanetTime + planetTimeSpan;
 
-        [Fact]
-        public void Repeat_ThrowsException_IfLengthIsZero()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => LoopRangeLong.Repeat(1, 0));
+            Assert.Equal(((long)end.Hour, (long)end.Minute), endPlanetTime.ToTime());
         }
 
         public static IEnumerable<object[]> GetOperatorPlusTestData()
         {
             yield return new object[] { 12, 0, 1, 0 };
+            yield return new object[] { 23, 59, 1, 0 };
+            yield return new object[] { 13, 17, 3, 3 };
+            yield return new object[] { 23, 59, 26, 17 };
+        }
+
+        [Theory]
+        [MemberData(nameof(GetOperatorMinusTestData))]
+        public void OperatorMinus_ReturnsCorrectValue(long startHour, long startMinute, long endHour, long endMinute)
+        {
+            var start = DateTime.UnixEpoch + new TimeSpan((int)startHour, (int)startMinute, 0);
+            var end = DateTime.UnixEpoch + new TimeSpan((int)endHour, (int)endMinute, 0);
+            if(end < start)
+            {
+                end = end.AddDays(1);
+            }
+            var span = end - start;
+
+            var startPlanetTime = new PlanetTime<EarthTimeConfiguration>(startHour, startMinute);
+            var endPlanetTime = new PlanetTime<EarthTimeConfiguration>(endHour, endMinute);
+            var planetTimeSpan = endPlanetTime - startPlanetTime;
+
+            Assert.Equal(((long)span.Hours, (long)span.Minutes), planetTimeSpan.ToTime());
+        }
+
+        public static IEnumerable<object[]> GetOperatorMinusTestData()
+        {
+            yield return new object[] { 12, 0, 13, 0 };
+            yield return new object[] { 23, 59, 1, 0 };
+            yield return new object[] { 13, 17, 3, 3 };
+            yield return new object[] { 23, 59, 0, 0 };
+        }
+
+        [Fact]
+        public void Ctor_ThrowsException_IfInvalidTime()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new PlanetTime<EarthTimeConfiguration>(24, 0));
         }
     }
 }
--- MarsMoons.Core/PlanetTime/PlanetTimeSpan.cs
+++ MarsMoons.Core/PlanetTime/PlanetTimeSpan.cs
@@ -2,10 +2,13 @@
 {
     public class PlanetTimeSpan<TTimeConfiguration> where TTimeConfiguration : IPlanetTimeConfiguration, new()
     {
-        private static IPlanetTimeConfiguration TimeConfiguration = new TTimeConfiguration();
+        private static readonly IPlanetTimeConfiguration TimeConfiguration = new TTimeConfiguration();
         internal readonly long _ticks;
 
         public PlanetTimeSpan(long ticks) => _ticks = ticks;
         public PlanetTimeSpan(long hours, long minutes) : this(TimeConfiguration.TimeSpanToTicks(hours, minutes)) { }
+
+        public (long hour, long minute) ToTime() =>
+            TimeConfiguration.TicksToTime(_ticks);
     }
 }
